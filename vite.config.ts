d43564cb8388e8affed47451a330f2c0c6263c96
jasonwong318 <85hvxj7vtv@privaterelay.appleid.com>
2026-05-31 01:12:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Testing/',
+  base: '/Therapist-Journey/',
   plugins: [
     react(),
     tailwindcss(),
